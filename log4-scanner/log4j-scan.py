2ddc36351d53f4603db18ca47041eaf5519fdf8d
Emre <et@mr.local>
2022-09-22 14:43:03 +0300

    added new callback provider

--- log4-scanner/log4j-scan.py
+++ log4-scanner/log4j-scan.py
@@ -262,6 +262,70 @@ class Interactsh:
                          }
         return new_log_entry
 
+class MrtrmnXyz:
+    # Source: https://github.com/knownsec/pocsuite3/blob/master/pocsuite3/modules/interactsh/__init__.py
+    def __init__(self, token="", server=""):
+        rsa = RSA.generate(2048)
+        self.public_key = rsa.publickey().exportKey()
+        self.private_key = rsa.exportKey()
+        self.token = token
+        self.server = server.lstrip('.') or 'mrtrmn.xyz'
+        self.headers = {
+            "Content-Type": "application/json",
+        }
+        if self.token:
+            self.headers['Authorization'] = self.token
+        self.secret = str(uuid4())
+        self.encoded = b64encode(self.public_key).decode("utf8")
+        guid = uuid4().hex.ljust(33, 'a')
+        guid = ''.join(i if i.isdigit() else chr(ord(i) + random.randint(0, 20)) for i in guid)
+        self.domain = f'{guid}.{self.server}'
+        self.correlation_id = self.domain[:20]
+
+        self.session = requests.session()
+        self.session.headers = self.headers
+        self.session.verify = False
+        self.session.proxies = proxies
+        self.register()
+
+    def register(self):
+        data = {
+            "public-key": self.encoded,
+            "secret-key": self.secret,
+            "correlation-id": self.correlation_id
+        }
+        res = self.session.post(
+            f"http://{self.server}/register", headers=self.headers, json=data, timeout=30)
+        if 'success' not in res.text:
+            raise Exception("Can not initiate interact.sh DNS callback client")
+
+    def pull_logs(self):
+        result = []
+        url = f"http://{self.server}/poll?id={self.correlation_id}&secret={self.secret}"
+        res = self.session.get(url, headers=self.headers, timeout=30).json()
+        aes_key, data_list = res['aes_key'], res['data']
+        for i in data_list:
+            decrypt_data = self.__decrypt_data(aes_key, i)
+            result.append(self.__parse_log(decrypt_data))
+        return result
+
+    def __decrypt_data(self, aes_key, data):
+        private_key = RSA.importKey(self.private_key)
+        cipher = PKCS1_OAEP.new(private_key, hashAlgo=SHA256)
+        aes_plain_key = cipher.decrypt(base64.b64decode(aes_key))
+        decode = base64.b64decode(data)
+        bs = AES.block_size
+        iv = decode[:bs]
+        cryptor = AES.new(key=aes_plain_key, mode=AES.MODE_CFB, IV=iv, segment_size=128)
+        plain_text = cryptor.decrypt(decode)
+        return json.loads(plain_text[16:])
+
+    def __parse_log(self, log_entry):
+        new_log_entry = {"timestamp": log_entry["timestamp"],
+                         "host": f'{log_entry["full-id"]}.{self.domain}',
+                         "remote_address": log_entry["remote-address"]
+                         }
+        return new_log_entry
 
 def parse_url(url):
     """
@@ -365,6 +429,8 @@ def main():
         cprint(f"[•] Initiating DNS callback server ({args.dns_callback_provider}).")
         if args.dns_callback_provider == "interact.sh":
             dns_callback = Interactsh()
+        if args.dns_callback_provider == "mrtrmn.xyz":
+            dns_callback = MrtrmnXyz()
         else:
             raise ValueError("Invalid DNS Callback provider")
         dns_callback_host = dns_callback.domain
